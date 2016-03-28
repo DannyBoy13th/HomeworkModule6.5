cb2053f5baee66c9547478a35b6c828d6f09ba80
DannyBoy13th <danielsolo13th@gmail.com>
2016-03-29 02:30:31 +0300

    Delete MusicInstrument.java

--- src/MusicStoreV3/MusicInstrument.java
+++
@@ -1,9 +0,0 @@
-package MusicStoreV3;
-
-abstract class MusicInstrument {
-   public abstract String getType();
-    @Override
-    public String toString(){
-        return "Instrument{" +getType()+"}";
-        }
-    }
